5f446ba3353831ab5a46de9ede8021eb99b40e5e
Simon <mashles@gmail.com>
2020-12-05 02:32:38 -0600

    Made the binary converter a single method

--- AdventOfCode/Day_05.cs
+++ AdventOfCode/Day_05.cs
@@ -9,24 +9,22 @@ namespace AdventOfCode
     public class Day_05 : BaseDay
     {
         private readonly List<string> _input;
-
         public Day_05()
         {
             _input = File.ReadAllLines(InputFilePath).ToList();
-            _input.Sort();
         }
 
         public override string Solve_1() => $"Solution to {ClassPrefix} {CalculateIndex()}, part 1: {Solve1()}";
-
         public override string Solve_2() => @$"Solution to {ClassPrefix} {CalculateIndex()}, part 2: {Solve2()}";
 
 
-        public long Solve1(){
+        public long Solve1()
+        {
             long result = 0;
             foreach(var input in _input)
             {
-                int row = GetRow(input.Substring(0, 7));
-                int column = GetColumn(input.Substring(7, 3));
+                int row = GetRowOrColumn(input.Substring(0, 7));
+                int column = GetRowOrColumn(input.Substring(7, 3));
                 var id = row*8+column;
                 if(id > result) { result = id; }
             }
@@ -39,8 +37,8 @@ namespace AdventOfCode
             var allIds = new List<int>();
             foreach(var input in _input)
             {
-                int row = GetRow(input.Substring(0, 7));
-                int column = GetColumn(input.Substring(7, 3));
+                int row = GetRowOrColumn(input.Substring(0, 7));
+                int column = GetRowOrColumn(input.Substring(7, 3));
                 var id = row*8+column;
                 allIds.Add(id);
             }
@@ -53,18 +51,11 @@ namespace AdventOfCode
             return result;
         }
 
-        public int GetRow(string rowLetters)
+        public int GetRowOrColumn(string letters)
         {
-            string binary = rowLetters.Replace("F","0").Replace("B","1");
-            int rowNumber = Convert.ToInt32(binary, 2);
-            return rowNumber;
-        }
-
-        public int GetColumn(string columnLetters)
-        {
-            string binary = columnLetters.Replace("L","0").Replace("R","1");
-            int columnNumber = Convert.ToInt32(binary, 2);
-            return columnNumber;
+            string binary = letters.Replace("F","0").Replace("B","1").Replace("L","0").Replace("R","1");
+            int number = Convert.ToInt32(binary, 2);
+            return number;
         }
     }
 }
