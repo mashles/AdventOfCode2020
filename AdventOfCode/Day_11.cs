314785b85ac3b92f02243a6db77aea8bfffacfab
Simon Hashem <simon.hashem@bluesource.net>
2020-12-11 21:00:55 -0600

    11 complete

--- AdventOfCode/Day_11.cs
+++ AdventOfCode/Day_11.cs
@@ -22,17 +22,15 @@ namespace AdventOfCode
 
         public long Solve1()
         {
-            var applyRulesCount = 0;
-            while(ApplyRules() > 0)
-            {
-                applyRulesCount++;
-            }
-            return applyRulesCount;
+            while (ApplyRules() > 0) ;
+            return _input.Sum(x => x.Count(y => y == '#'));
         }
 
         public long Solve2()
         {
-            return 0;
+            _input = File.ReadAllLines(InputFilePath).ToList();
+            while (ApplyRules2() > 0) ;
+            return _input.Sum(x => x.Count(y => y == '#'));
         }
 
 
@@ -51,7 +49,28 @@ namespace AdventOfCode
                         changeCount++;
                     }
                 }
-                newMap.Add(newRow.ToString());
+                newMap.Add(new string(newRow));
+            }
+            _input = newMap;
+            return changeCount;
+        }
+
+        public int ApplyRules2()
+        {
+            var changeCount = 0;
+            var newMap = new List<string>();
+            for (int i = 0; i < _input.Count; i++)
+            {
+                var newRow = new char[_lineLength];
+                for (int c = 0; c < newRow.Length; c++)
+                {
+                    newRow[c] = GetNewSeatState2(c, i);
+                    if (newRow[c] != _input[i][c])
+                    {
+                        changeCount++;
+                    }
+                }
+                newMap.Add(new string(newRow));
             }
             _input = newMap;
             return changeCount;
@@ -74,17 +93,70 @@ namespace AdventOfCode
             return currentState;
         }
 
+        public char GetNewSeatState2(int x, int y)
+        {
+            var currentState = _input[y][x];
+            switch (currentState)
+            {
+                case 'L':
+                    if (GetVisibleCount(x, y) == 0)
+                        return '#';
+                    break;
+                case '#':
+                    if (GetVisibleCount(x, y) >= 5)
+                        return 'L';
+                    break;
+            }
+            return currentState;
+        }
+
+        public int GetVisibleCount(int x, int y)
+        {
+            int count = 0;
+            
+
+            for(int i = -1; i <= 1; i++)
+            {
+                for (int j = -1; j <= 1; j++)
+                {
+                    if (!(i == 0 && j == 0))
+                    {
+                        var currX = x;
+                        var currY = y;
+                        bool hit = false;
+                        char hitChar = '.';
+                        while (!hit && currX < _lineLength && currX >= 0 && currY < _input.Count && currY >= 0)
+                        {
+                            if (!(currY == y && currX == x) && _input[currY][currX] != '.')
+                            {
+                                hit = true;
+                                hitChar = _input[currY][currX];
+                            }
+                            currX = currX += j;
+                            currY = currY += i;
+                        }
+                        if (hit == true && hitChar == '#')
+                        {
+                            count++;
+                        }
+                    }
+                }
+            }
+            return count;
+        }
+
         public int GetAdjacentCount(int x, int y)
         {
             int count = 0;
             var yRange = (Math.Max(0, y-1), Math.Min(_input.Count-1, y+1));
             var xRange = (Math.Max(0, x-1), Math.Min(_lineLength-1, x+1));
 
-            for(int iy = yRange.Item1; iy < yRange.Item2; iy++)
+            for(int iy = yRange.Item1; iy <= yRange.Item2; iy++)
             {
-                for(int ix = xRange.Item1; ix < xRange.Item2; ix++)
+                for(int ix = xRange.Item1; ix <= xRange.Item2; ix++)
                 {
-                    if(iy != y && ix != x && _input[iy][ix] == '#')
+                    if(iy == y && ix == x) { continue; }
+                    if(_input[iy][ix] == '#')
                     {
                         count++;
                     }
